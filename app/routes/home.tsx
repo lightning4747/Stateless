7cdcf608e2cad4a4ca24d2791d30796ce18ee8d8
lightning4747 <vignesh112847@gmail.com>
2026-02-14 15:08:50 +0530

    Redirect page after the file uplaod

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,6 +3,7 @@ import type { Route } from "./+types/home";
 import Navbar from "components/Navbar";
 import Button from "components/ui/Button"
 import Upload from "components/Upload";
+import { useNavigate } from "react-router";
 
 export function meta({}: Route.MetaArgs) {
   return [
@@ -12,6 +13,15 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const handleUploadComplete = async (base64Image: string) => {
+    const newId = Date.now().toString();
+    navigate(`/visualizer/${newId}`);
+
+    return true;
+  }
+
   return (
     <div className="home">
       <Navbar/>
@@ -50,10 +60,7 @@ export default function Home() {
                   <h3>Upload your floor plan</h3>
                   <p>Support JPG, PNG, formats up to 10MB</p>
                 </div>
-                <Upload onComplete={(base64Data) => {
-                  console.log("Upload complete:", base64Data);
-                }} />
-
+                <Upload onComplete={handleUploadComplete}/>
               </div>
           </div>
       </section>
